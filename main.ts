f29453e2a172b4abef62d5b86b7ea0e14dda1ec4
Ravish Fayyaz <160616153+ravishfayyaz555@users.noreply.github.com>
2024-06-23 18:56:41 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,80 @@
+#! usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+
+class Studant {
+    name:string
+    constructor(n:string){
+        this.name=n
+    };
+};
+class Person{
+    studants:Studant[]=[];
+    addStudant(obj:Studant){
+        this.studants.push(obj)
+    };
+};
+
+const person = new Person();
+
+const programStart =async(person:Person)=>{
+    
+        console.log(
+            chalk.yellowBright("      \n\t********  WELCOME  **********\n\t    ")
+        );
+        do{
+        const ans = await inquirer.prompt({
+            name:"select",
+            type:"list",
+            message:chalk.greenBright("Whom would you like to interact with?"),
+            choices: ["staff","studant","exit"]
+        });
+
+        if(ans.select == "staff"){
+            console.log(
+                chalk.redBright("You approach the staff room. Please feel free to ask any qustion.")
+            );
+
+        }else if(ans.select == "studant"){
+            const ans = await inquirer.prompt({
+                name:"studant",
+                type:"input",
+                message:chalk.greenBright("Enter the studant's name you wish to engage with:")
+            });
+            const studant =person.studants.find(val => val.name == ans.studant)
+            if(!studant){
+                const name = new Studant(ans.studant);
+                person.addStudant(name);
+                console.log(
+                    chalk.yellow(`Hello i am ${name.name}. Nice to meet you!`)
+                );
+                console.log(
+                    chalk.yellowBright("New stusant added")
+                );
+                console.log(chalk.yellow("Current studant list:")
+            );
+                console.log(person.studants);
+
+            }else{
+                console.log(chalk.yellowBright
+                    (`Hello i am ${studant.name}. Nice to see you again!`)
+                );
+                console.log(
+                    chalk.yellowBright
+                    ("Existing student list:")
+                );
+                console.log(person.studants);
+            }
+        }else if (ans.select == "exit"){
+            console.log(chalk.redBright
+                ("Exiting the program...")
+            );
+            process.exit()
+        }
+    }while(true);
+
+}
+
+programStart(person);
